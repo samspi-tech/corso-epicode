be874e0b925cad342bc37ab74daaee226269fb56
Andrea <andrew.ryan.r.a@gmail.com>
2024-12-17 23:26:53 +0100

    M2[W1D1] Exercise.

--- m2/w1/d1/script.js
+++ m2/w1/d1/script.js
@@ -1,8 +1,11 @@
 let age = 32;
 let driverLicense = age >= 18 ? true : false;
+let teenAge = 18 - age;
 
 if (driverLicense === true && age >= 18) {
   console.log("You can drive a car.");
 } else {
-  console.log(`Sorry, wait another ${18 - age} years.`);
+  console.log(
+    `Sorry, wait another ${teenAge !== 1 ? `${teenAge} years` : "year."}`
+  );
 }
